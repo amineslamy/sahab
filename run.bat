567bc79617e5c16c1909bce0ae1de05d2eed2475
amineslamy <amin.es123@gmail.com>
2026-08-13 18:11:15 +0330

    اجرای اولیه به صورت exe در آمد

--- run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File ".\Start.ps1"
--- start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start "" "pocketbase.exe" serve
-timeout /t 2 /nobreak >nul
-start http://127.0.0.1:8090
